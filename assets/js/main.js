5c0734d1cb3a3c4eda98fd5e9682f529f7b0577b
meishafifah <meishafifah@students.amikom.ac.id>
2022-02-03 17:44:13 +0700

    kurang login register

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-var sidebarToggler = document.getElementById('sidebar-toggler')
-sidebarToggler.onclick = function () {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("show-sidebar");
-    
-    var bodyOverlay = document.getElementById("body-overlay");
-    bodyOverlay.classList.toggle("body-overlay-show");
+var buttonSidebar = document.getElementById('sidebar-toggler')
+
+buttonSidebar.onclick = function() {
+    var elementSidebar = document.getElementById('sidebar')
+    elementSidebar.classList.toggle('show-sidebar')
+
+    var bodyOverlay = document.getElementById('body-overlay')
+    bodyOverlay.classList.toggle('body-overlay-show')
 }
 
 var bodyOverlay = document.getElementById('body-overlay')
-bodyOverlay.onclick = function () {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("show-sidebar");
-    
-    var bodyOverlay = document.getElementById("body-overlay");
-    bodyOverlay.classList.toggle("body-overlay-show");
+bodyOverlay.onclick = function() {
+    var elementSidebar = document.getElementById('sidebar')
+    elementSidebar.classList.toggle('show-sidebar')
+
+    var bodyOverlay = document.getElementById('body-overlay')
+    bodyOverlay.classList.toggle('body-overlay-show')
 }
