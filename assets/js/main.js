7473a32eb75f294f5defaa45dbfb6d872cde44c9
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-02-05 12:48:44 +0700

    update asset

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,21 @@ bodyOverlay.onclick = function() {
     var bodyOverlay = document.getElementById('body-overlay')
     bodyOverlay.classList.toggle('body-overlay-show')
 }
+
+var nameTagIcon = document.getElementById('name-tag-icon-zaki')
+nameTagIcon.onclick = function() {
+    var nameTag = document.getElementById('name-tag-zaki')
+    nameTag.classList.toggle('opacity-1')
+}
+
+var nameTagIcon = document.getElementById('name-tag-icon-okta')
+nameTagIcon.onclick = function() {
+    var nameTag = document.getElementById('name-tag-okta')
+    nameTag.classList.toggle('opacity-1')
+}
+
+var nameTagIcon = document.getElementById('name-tag-icon-meisha')
+nameTagIcon.onclick = function() {
+    var nameTag = document.getElementById('name-tag-meisha')
+    nameTag.classList.toggle('opacity-1')
+}
